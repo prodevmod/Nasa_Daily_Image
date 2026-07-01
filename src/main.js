7e04d7480d4bab634cf6c33344f0ee5c0ab55500
prodevmod <rosandrox12@gmail.com>
2026-07-01 13:18:58 +0300

    extra touch ups (fonts+glow cursor)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+const today = new Date().toISOString().split("T")[0];
 
-document.querySelector("#app").innerHTML = "<p>loading...</p>";
+document.querySelector("#app").innerHTML = `<div class="loading"><p>loading...</p>
+<p class="subtitles"> <br>
+Communicating directly with the ISS...<br>
+Fetching required files...</p></div>`;
 
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
   .then(response => {
@@ -26,16 +30,26 @@ fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
     }
 
     document.querySelector("#app").innerHTML = `
-      <h1>${data.title}</h1>
+    <div class="basic_data">
+      <h1 id="title">${data.title}</h1>
       ${media}
-      <p>${data.explanation}</p>
-    `;
+      <p id="desc">${data.explanation}</p>
+    </div>`;
   })
   .catch(err => {
     document.querySelector("#app").innerHTML = `
+    <div class="loading">
       <p>
         <strong>NASA API Error:</strong> ${err.message}<br>
         <small>Please try refreshing the page in a few moments.</small>
       </p>
+      </div>
     `;
+
   });
+
+window.addEventListener('mousemove', (e) => {
+    // This updates the global --x and --y variables on the body tag dynamically
+    document.body.style.setProperty('--x', `${e.clientX}px`);
+    document.body.style.setProperty('--y', `${e.clientY}px`);
+});
